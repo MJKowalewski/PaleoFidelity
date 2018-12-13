ed168e1244219d2d59ad8bf787f5575c66a90be8
Michal Kowalewski <kowalewski@ufl.edu>
2018-12-13 13:53:32 -0500

    Version 0.3.0

--- R/AlphaPlot.R
+++ R/AlphaPlot.R
@@ -1,4 +1,4 @@
-#' AlphaPlot (outputs a cross plot of live-dead offsets in alpha diversity and evenness)
+#' AlphaPlot (a cross plot of live-dead offsets in alpha diversity and evenness)
 #'
 #' AlphaPlot function generates a bivariate plot of an offset in alpha diversity (DELTA S)
 #' on x axis versus an offset in evenness (DELTA PIE) for all live-dead pairwise comparisons.
@@ -16,14 +16,6 @@
 #'
 #' @param x An object (a list) returned by FidelityDiv function
 #'
-#' @param CI A numerical value (default = 0.5) defining confidence bars for individual sites.
-#'  Note: 0.5 - plots bars represnting interuqartile range, 0.95 - plots 95% confidence bars, etc.
-#'  Confidence bars are estimated as percentiles of subsampled estimates of Delta S and Delta PIE.
-#'
-#' @param CImean A numerical value (default = 0.99) defining confidence bars for means of all sites
-#'  or groups of sites (if 'gp' factor was provided). Note: 0.5 - plots bars represnting interuqartile
-#'  range, 0.95 - plots 95% confidence bars, etc. Confidence bars are estimated as percentiles of
-#'  subsampled estimates of Delta S and Delta PIE.
 #'
 #' @param colpt A character string or number defining color of symbols for individual sites
 #' (default = 'gray')
@@ -34,70 +26,75 @@
 #' @param bgpt A character string or number defining color of symbols for individual
 #' sites (default = 'white')
 #'
-#' @param pchpt A number defining symbol type (default = 21)
-#' Note: filled symbol numbers (21-25) should be used
+#' @param cf.bars Logical (default=TRUE) to indicate if confidence bars should be plotted
+#'
+#' @param cex a numerical value (default=0.8) defining expansion factor for symbols
+#'
+#' @param pchpt A number defining symbol type (default = 21) (filled symbols (21-25) should be used)
 #'
 #' @param colmean A character string or number defining color of bars representing
 #' mean estimates for all sites (default = 'white')
 #'
+#' @param col.gp A character string or number defining colors for group (default = 1:#levels)
+#' for symbols and bars for sites (applicable when 'gp' factor is provided). The number of colors
+#' must match number of levels in a grouping factor
 #'
-#' @param colpt.gp A character string or number defining colors by group (default = 1:#levels)
-#' for symbols for sites (applicable when 'gp' factor is provided). Custom colors
-#' can be provided, but must match number of levels in a grouping factor
-#'
-#' @param colci.gp A character string or number defining colors by group (default = 1:#levels)
-#' for confidence bars for sites (applicable when 'gp' factor is provided). Custom colors
-#' can be provided, but must match number of levels in a grouping factor
-#'
-#'
-#' @param addlegend Logical (default=TRUE): add legend (activated only when 'gp' factor provided)
-#'
+#' @param addlegend Logical (default=TRUE): add legend
+#' Note: The legend is plotted only when 'gp' factor provided
 #'
 #' @return A single bivariate plot produce by plot function
 #'
 #'
 #' @examples
 #'
-#' AlphaPlot(FidelityDiv(FidData$live, FidData$dead, FidData$habitat, n.filters=30, t.filters=1))
+#' my.fid1 <- FidelityDiv(FidData$live, FidData$dead, n.filters=30)
+#' AlphaPlot(my.fid1)
+#' my.fid2 <- FidelityDiv(FidData$live, FidData$dead, FidData$habitat, n.filters=50, iter=1000, CI=0.95)
+#' AlphaPlot(my.fid2, col.gp=c('forestgreen', 'coral1'), bgpt='beige')
 #'
 #' @export
 
-AlphaPlot <- function(x, CI=0.5, CImean=0.99, colpt='gray', colci='gray', bgpt='white',
-                      pchpt=21, colmean='black', colpt.gp=NULL, colci.gp=NULL,
-                      addlegend=TRUE) {
-  z <- x$out
-  samDS <- rowMeans(z[,1,])
-  samDScf <- t(apply(z[,1,], 1, stats::quantile, prob=c((1-CI)/2, 1 - (1-CI)/2)))
-  samDP <- rowMeans(z[,2,])
-  samDPcf <- t(apply(z[,2,], 1, stats::quantile, prob=c((1-CI)/2, 1 - (1-CI)/2)))
-  meanDS <- mean(colMeans(z[,1,]))
-  meanDP <- mean(colMeans(z[,2,]))
-  DScf <- stats::quantile(colMeans(z[,1,]), prob=c((1-CImean)/2, 1 - (1-CImean)/2))
-  DPcf <- stats::quantile(colMeans(z[,2,]), prob=c((1-CImean)/2, 1 - (1-CImean)/2))
-  xmax <- max(abs(range(samDScf)))
-  ymax <- max(abs(range(samDPcf)))
-
-  graphics::plot(samDS, samDP, xlim=c(-xmax, xmax), ylim=c(-ymax, ymax), type='n', las=1,
+AlphaPlot <- function(x, colpt='gray', colci='gray', bgpt='white', cf.bars=TRUE, cex=0.8,
+                      pchpt=21, colmean='black', col.gp=NULL, addlegend=TRUE) {
+  xmax <- 1.2 * max(abs(x$x[,2]))
+  ymax <- 1.2 * max(abs(x$y[,2]))
+  graphics::plot(x$x[,2], x$y[,2], xlim=c(-xmax, xmax), ylim=c(-ymax, ymax), type='n', las=1,
        xlab=bquote(Delta['S']), ylab=bquote(Delta['PIE']), cex.lab=1.5)
   graphics::abline(h=0, v=0, lwd=1, col='gray')
   if (length(x$gp)==0) {
-   for(i in 1:length(samDS)) {
-     graphics::points(cbind(cbind(rep(samDS[i], 2)), cbind(samDPcf[i,])), type='l', col=colci, lwd=0.5, lend=3)
-     graphics::points(cbind(cbind(samDScf[i,]), cbind(rep(samDP[i],2))), type='l', col=colci, lwd=0.5, lend=3)
+    if (cf.bars) {
+     for(i in 1:length(x$x[,2])) {
+     graphics::points(cbind(cbind(rep(x$x[i,2], 2)), cbind(x$y[i,3:4])), type='l', col=colci, lwd=0.5, lend=3)
+     graphics::points(cbind(cbind(x$x[i,3:4]), cbind(rep(x$y[i,2],2))), type='l', col=colci, lwd=0.5, lend=3)
      }
-    graphics::points(samDS, samDP, pch=pchpt, col=colpt, bg=bgpt, cex=0.8)
+    }
+    graphics::points(x$x[,2], x$y[,2], pch=pchpt, col=colpt, bg=bgpt, cex=cex)
   }
   if (length(x$gp)>0) {
-    ifelse(length(colci.gp) > 0, colci.gp <- colci.gp, colci.gp <- 1:(length(levels(x$gp))))
-    barcols <- colci.gp[x$gp]
-    for(i in 1:length(samDS)) {
-      graphics::points(cbind(cbind(rep(samDS[i], 2)), cbind(samDPcf[i,])), type='l', col=barcols[i], lwd=0.5, lend=3)
-      graphics::points(cbind(cbind(samDScf[i,]), cbind(rep(samDP[i],2))), type='l', col=barcols[i], lwd=0.5, lend=3)
+    ifelse(length(col.gp) > 0, col.gp <- col.gp, col.gp <- 1:(length(levels(x$gp))))
+    barcols <- col.gp[x$gp]
+    if (cf.bars) {
+     for(i in 1:length(x$x[,2])) {
+      graphics::points(cbind(cbind(rep(x$x[i,2], 2)), cbind(x$y[i,3:4])), type='l', col=barcols[i], lwd=0.5, lend=3)
+      graphics::points(cbind(cbind(x$x[i,3:4]), cbind(rep(x$y[i,2],2))), type='l', col=barcols[i], lwd=0.5, lend=3)
+     }
     }
-    graphics::points(samDS, samDP, pch=pchpt, col=barcols, bg=bgpt, cex=0.8)
+    graphics::points(x$x[,2], x$y[,2], pch=pchpt, col=barcols, bg=bgpt, cex=cex)
   }
-  graphics::points(rep(meanDS, 2), DPcf, type='l', col=colmean, lwd=2, lend=3)
-  graphics::points(DScf, rep(meanDP, 2), type='l', col=colmean, lwd=2, lend=3)
-  if (addlegend & length(x$gp) > 0) graphics::legend('topleft', pch=pchpt, col=colci.gp, levels(x$gp))
+
+  if (length(x$xgp) == 0) {
+   graphics::points(rep(x$xmean[1], 2), x$ymean[2:3], type='l', col=colmean, lwd=4, lend=3)
+   graphics::points(x$xmean[2:3], rep(x$ymean[1], 2), type='l', col=colmean, lwd=4, lend=3)
+  }
+
+  if (length(x$xgp)>0) {
+   for(i in 1:nrow(x$xgp)) {
+   graphics::points(rep(x$xgp[i,2], 2), x$ygp[i,3:4], type='l', col=col.gp[i], lwd=4, lend=3)
+   graphics::points(x$xgp[i,3:4], rep(x$ygp[i,2], 2), type='l', col=col.gp[i], lwd=4, lend=3)
+   }
+  }
+
+  if (addlegend & length(x$gp) > 0) graphics::legend('topleft', pch=pchpt, pt.bg=bgpt,
+                                                     col=col.gp, levels(x$gp), pt.cex=cex)
   graphics::box()
 }
--- R/FidelityDiv.R
+++ R/FidelityDiv.R
@@ -1,4 +1,4 @@
-#' FidelityDiv estimates live-dead differences in alpha and beta diversity
+#' FidelityDiv estimates live-dead differences in alpha diversity and evenness
 #'
 #' FidelityDiv provides estimates of differences in diversity between live and dead samples
 #' using matrices (live and dead) with species/taxon abundance data. In the case of datastes
@@ -6,22 +6,19 @@
 #' If 'gp' factor is provided to aggregate sets of sites/samples, means for groups are returned as well.
 #'
 #'@details FidelityDiv assess live-dead offsets in evenness/diversity using measures
-#' of alpha diversity, alpha evenness, and beta diversity (returned as 3 separate objects):
+#' of alpha diversity and alpha evenness (returned as 2 separate objects):
 #'
 #' (1) x - Live-dead offsets in alpha diversity for individual sites. The difference is measured as
 #' ln(S)DEAD - ln(S)LIVE (i.e., difference between natural logarithms of sample-standardized
 #' species richness of dead and live samples). A negative value indicates that alpha diversity
 #' of live samples exceeds alpha diversity of dead sample (and vice versa).
+#' Confidence intervals and p.values for Null H: Delta S = 0 are also reported.
 #'
 #' (2) y - Live-dead offsets in evenness for individual sites. The difference is measured as
 #' PIE(DEAD) - PIE(LIVE) (i.e., difference between estimates of Hurlbert's PIE for live and
 #' dead samples). A negative value indicates that evenness of live samples exceeds evenness
 #' of dead sample (and vice versa).
-#'
-#' (3) z - Live-dead offsets in beta diversity for individual sites. The offset is estimated as
-#'  Beta(DEAD) - Beta(LIVE). Three  measures of beta diversity can be selected:
-#'  "beta variance", "Shannon's beta", "Whittaker's beta". A negative value indicates that
-#'  beta diversity of live sample exceed beta diversity of dead sample (and vice versa).
+#' Confidence intervals and p.values for Null H: Delta S = 0 are also reported.
 #'
 #'
 #' @param live A species/taxon abundance matrix with counts of live-collected specimens
@@ -39,56 +36,119 @@
 #'
 #' @param t.filters An integer used to filter out rare taxa (default t.filters=1, taxa >= 1 occurrence kept)
 #'
-#' @param iter An integer defining number of resampling iteration (default iter=0)
+#' @param iter An integer defining number of resampling iteration (default iter=100)
 #'
+#' @param CI A numerical value (default = 0.5) defining confidence bars for individual sites.
+#'  Note: 0.5 - plots bars represnting inter-quartile ranges, 0.95 - plots 95% confidence bars, etc.
+#'  Confidence bars are estimated as percentiles of subsampled estimates of Delta S and Delta PIE.
+#'
+#' @param CImean A numerical value (default = 0.99) defining confidence bars for means of all sites
+#'  or groups of sites (if 'gp' factor was provided). Note: 0.5 - plots bars represnting inter-quartile
+#'  range, 0.95 - plots 95% confidence bars, etc. Confidence bars are estimated as percentiles of
+#'  subsampled estimates of Delta S and Delta PIE.
+#'
+#' @param outdata Logical (default = FALSE) to determine if data files should be included in the output
 #'
 #' @return A list containing the following components:
+#'   \item{live}{The post-processed version of 'live' data matrix used in all analyses}
+#'   \item{dead}{The post-processed version of 'dead' data matrix used in all analyses}
+#'   \item{gp}{The post-processed version of 'gp' factor, when provided}
 #'   \item{x}{DELTA S values for each live-dead comparisons (site-level differences in sample standardized
 #'            species richness)}
 #'   \item{y}{DELTA PIE values for each live-dead comparisons (site-level differences in evenness
 #'            estimated as Hurlbert's PIE)}
-#'   \item{z}{DELTA BETA Live-dead fidelity index for each live-dead comparison}
-#'   \item{observed.means}{Grand means of diversity measures (means of  $x, $y, & $z) and
-#'        by-group means when gp factor provided}
-#'   \item{live}{The post-processed version of 'live' data matrix used in all analyses}
-#'   \item{dead}{The post-processed version of 'dead' data matrix used in all analyses}
-#'   \item{gp}{The post-processed version of 'gp' factor, when provided}
+#'   \item{xmean}{Grand mean of Delta S}
+#'   \item{ymean}{Grand mean of Delta PIE}
+#'   \item{xmean}{Grand mean of Delta S}
+#'   \item{xgp}{Group means of Delta S (when 'gp' factor provided)}
+#'   \item{ygp}{Group means of Delta PIE (when 'gp' factor provided)}
+#'   \item{p.values}{p.values for Null H: Delta.S.p=0 and Delta.PIE.p=0}
+#'   \item{p.gps}{per-group p.values for Null H: Delta.S.p=0 and Delta.PIE.p=0}
 #'
 #' @examples
 #'
-#' data(FidData)
-#' out1 <- FidelityDiv(live = FidData$live, dead = FidData$dead, gp = FidData$habitat)
-#'
+#' FidelityDiv(FidData$live, FidData$dead, n.filters=30)
+#' my.fid <- FidelityDiv(FidData$live, FidData$dead, FidData$habitat, n.filters=50, iter=1000, CI=0.95)
+#' my.fid$x # site-level estimates of Delta S with 95% CIs and p values
+#' my.fid$p.gps # p values for means of groups
 #'
 #' @export
 #'
 #' @importFrom vegan rrarefy
 
-FidelityDiv <- function(live, dead, gp=NULL, report=FALSE, n.filters=0, t.filters=1, iter=100)
+FidelityDiv <- function(live, dead, gp=NULL, report=FALSE, n.filters=0, t.filters=1,
+                        iter=100, CI=0.5, CImean=0.99, outdata=FALSE)
 {
 
   # 1. Data assessment and filtering
   out <- FidelitySummary(live, dead, gp, report=report, n.filters=n.filters, t.filters=t.filters) # check/filter data
-  if (length(out)==2) {live <- out$live;  dead <- out$dead}
-  if (length(out)==3) {live <- out$live;  dead <- out$dead; gp <- out$gp}
+  if (length(out) == 2) {live <- out$live;  dead <- out$dead}
+  if (length(out) == 3) {live <- out$live;  dead <- out$dead; gp <- out$gp}
 
-  # 2. Alpha Diversity
+  # 2. Alpha Diversity/Evenness
   min.sam <- apply(cbind(rowSums(live), rowSums(dead)), 1, min)
-  pie.f <- function(x) (sum(x>0) / (sum(x>0) - 1)) * (1 - sum((x/sum(x))^2))
+  pie.f <- function(x) (sum(x > 0) / (sum(x > 0) - 1)) * (1 - sum((x / sum(x)) ^ 2))
   delta.alpha <- function(x, y, min) {
     a <- vegan::rrarefy(x, sample=min)
     b <- vegan::rrarefy(y, sample=min)
     cbind(log(apply(b, 1, function(z) sum(z > 0))) - log(apply(a, 1, function(z) sum(z > 0))),
     apply(b, 1, pie.f) - apply(a, 1, pie.f))
   }
-  out.alpha <- array(NA, dim=c(nrow(live), 2, iter))
-  for (i in 1:iter) out.alpha[,,i] <- delta.alpha(live, dead, min.sam)
+  out1 <- array(NA, dim=c(nrow(live), 2, iter))
+  for (i in 1:iter) out1[,,i] <- delta.alpha(live, dead, min.sam)
+  p1DS <- apply(out1[,1,], 1, function(x) sum(x > 0))
+  p2DS <- apply(out1[,1,], 1, function(x) sum(x < 0))
+  p1DP <- apply(out1[,2,], 1, function(x) sum(x > 0))
+  p2DP <- apply(out1[,2,], 1, function(x) sum(x < 0))
+  p.DS <- 2 * apply(cbind(p1DS, p2DS), 1, min) / iter
+  p.DP <- 2 * apply(cbind(p1DP, p2DP), 1, min) / iter
+  if (sum(p.DS == 0) > 0) p.DS[p.DS == 0] <- 1 / iter
+  if (sum(p.DP == 0) > 0) p.DP[p.DP == 0] <- 1 / iter
+  DS <- cbind(n.std=min.sam, est=rowMeans(out1[,1,]),
+              t(apply(out1[,1,], 1, stats::quantile, prob=c((1 - CI) / 2, 1 - (1 - CI) / 2))),
+              p=p.DS)
+  DP <- cbind(n.std=min.sam, est=rowMeans(out1[,2,]),
+              t(apply(out1[,2,], 1, stats::quantile, prob=c((1 - CI) / 2, 1 - (1 - CI) / 2))),
+              p=p.DP)
 
-  # 3. OUTPUT
-  #  ifelse(length(gp) > 0, rep1 <- rbind(total=mean.measures, mean.gp), rep1 <- mean.measures)
-  #  ifelse(length(gp) > 0, rep2 <- rbind(PF.stats, PF.stats.gp), rep2 <- PF.stats)
-  out1 <- list(x='x', y='y', z='z', observed.means='obs.mean', live=live, dead=dead, gp=gp,
-               out=out.alpha)
+  # 3.Means for all data and by groups (if 'gp' factor provided)
+  # All data
+  allS <- colMeans(out1[,1,])
+  allP <- colMeans(out1[,2,])
+  meanDS <- c(mean(allS), stats::quantile(allS, prob=c((1-CImean)/2, 1 - (1-CImean)/2)))
+  meanDP <- c(mean(allP), stats::quantile(allP, prob=c((1-CImean)/2, 1 - (1-CImean)/2)))
+  allpS <- 2 * min(sum(allS > 0), sum(allS < 0))
+  ifelse(allpS == 0, Delta.S.p <- 1 / iter, Delta.S.p <- allpS)
+  allpP <- 2 * min(sum(allP > 0), sum(allP < 0))
+  ifelse(allpP == 0, Delta.PIE.p <- 1 / iter, Delta.PIE.p <- allpP)
+  # By groups
+  if (length(gp) > 0) {
+   outDS <- sapply(as.data.frame(out1[,1,]), function(x) tapply(x, gp, mean))
+   outDS2 <- cbind(n.sites=tapply(gp, gp, length), est=apply(outDS, 1, mean),
+            t(apply(outDS, 1, stats::quantile, prob=c((1-CImean)/2, 1 - (1-CImean)/2))))
+   outDP <- sapply(as.data.frame(out1[,2,]), function(x) tapply(x, gp, mean))
+   outDP2 <- cbind(n.sites=tapply(gp, gp, length), est=apply(outDP, 1, mean),
+                  t(apply(outDP, 1, stats::quantile, prob=c((1-CImean)/2, 1 - (1-CImean)/2))))
+   gpDS1 <- apply(outDS, 1, function(x) sum(x<0))
+   gpDS2 <- apply(outDS, 1, function(x) sum(x>0))
+   gpDP1 <- apply(outDP, 1, function(x) sum(x<0))
+   gpDP2 <- apply(outDP, 1, function(x) sum(x>0))
+   p.gp.DS <- 2 *  apply(cbind(gpDS1, gpDS2), 1, min) / iter
+   p.gp.DP <- 2 *  apply(cbind(gpDP1, gpDP2), 1, min) / iter
+   if (sum(p.gp.DS == 0) > 0) p.gp.DS[p.gp.DS == 0] <- 1 / iter
+   if (sum(p.gp.DP == 0) > 0) p.gp.DP[p.gp.DP == 0] <- 1 / iter
+  }
+outDS3 <- NULL
+outDP3 <- NULL
+p.GP <- NULL
+if (length(gp) > 0) outDS3 <- outDS2
+if (length(gp) > 0) outDP3 <- outDP2
+if (length(gp) > 0)  p.GP <- cbind(p.Delta.S=p.gp.DS, p.Delta.PIE=p.gp.DP)
+
+if (outdata) out1 <- list(live=live, dead=dead, gp=gp, out=out1, x=DS, y=DP, xmean=meanDS, ymean=meanDP,
+                          xgp=outDS3, ygp=outDP3, p.values=cbind(Delta.S.p, Delta.PIE.p))
+if (!outdata) out1 <- list(gp=gp, x=DS, y=DP, xmean=meanDS, ymean=meanDP, xgp=outDS3, ygp=outDP3,
+                           p.values=cbind(Delta.S.p, Delta.PIE.p), p.gps=p.GP)
   return(out1)
 }
 
